54ad50f159c4f3c554ae4c30e457619308176996
Greg Stanton <highermathhelp@gmail.com>
2025-12-16 23:19:49 -0700

    Implement initial Vector class and test suite

--- src/Vector.js
+++ src/Vector.js
@@ -2,16 +2,87 @@
  * @module Vector
  */
 
-/* Requirements:
- * array()
- * dist(v)
- * add(v), sub(v), mult(n) (or static)
- * copy()
- * equals(v)
- * mag()
-*/
+/**
+ * A minimal class representing a vector with three components. Only behavior required for
+ * the RMF Engine's proof of concept is supported.
+ * 
+ * Note: For simplicity and performance, parameters are not validated.
+ */
+export class Vector {
+  #array;
 
-// just including this temporarily to make sure Vitest is set up properly
-export function sum(a, b) {
-  return a + b
+  /** 
+  * Create a vector.
+  * @param {number} x  - The x-component.
+  * @param {number} y  - The y-component.
+  * @param {number} z  - The z-component.
+  */
+  constructor(x, y, z) {
+    this.#array = [x, y, z];
+  }
+
+  /**
+   * Convert the vector to an array. Returns a copy, not a reference, 
+   * to prevent corruption of the underlying data.
+   * @return {number[]} An array containing the components of the vector.
+   */
+  array() {
+    return [...this.#array];
+  }
+
+  /**
+   * The x-component of the vector.
+   * @type {number}
+   */
+  get x() {
+    return this.#array[0];
+  }
+
+  set x(value) {
+      this.#array[0] = value;
+  }
+
+  /**
+   * The y-component of the vector.
+   * @type {number}
+   */
+  get y() {
+    return this.#array[1];
+  }
+
+  set y(value) {
+      this.#array[1] = value;
+  }
+
+  /**
+   * The z-component of the vector.
+   * @type {number}
+   */
+  get z() {
+    return this.#array[2];
+  }
+
+  set z(value) {
+      this.#array[2] = value;
+  }
+
+  /**
+   * The distance between the vector and a given vector.
+   * @param {Vector} vector - The given vector.
+   * @return {number} - The distance.
+   */
+  dist(vector) {
+    return Math.hypot(this.x - vector.x, this.y - vector.y, this.z - vector.z);
+  }
+
+  /**
+   * Returns `true` if the two vectors have the same components and `false` otherwise.
+   * @param {Vector} vector - The given vector for comparison.
+   * @return {boolean} - The truth value indicating whether the vectors are equal.
+   */
+  equals(v) {
+    return this.x === v.x && this.y === v.y && this.z === v.z;
+  }
+
+  // TODO: Add a method for approximate equality
 }
--- tests/Vector.test.js
+++ tests/Vector.test.js
@@ -1,7 +1,77 @@
 import { expect, test } from 'vitest'
-import { sum } from '../src/Vector.js'
+import { Vector } from '../src/Vector.js'
 
-// just making sure Vitest works (will delete this later)
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3)
+test('vector.array() gets [2, 3, 5] from vector <2, 3, 5>', () => {
+  const vector = new Vector(2, 3, 5);
+  expect(vector.array()).toEqual([2, 3, 5]);
+})
+
+test('mutating vector.array() does not mutate internal array', () => {
+  const vector = new Vector(2, 3, 5);
+  const array = vector.array();
+  array[0] = 1;
+  expect(vector.array()).toEqual([2, 3, 5]);
+})
+
+test('vector.x gets 2 from vector <2, 3, 5>', () => {
+  const vector = new Vector(2, 3, 5);
+  expect(vector.x).toBe(2);
+})
+
+test('setting vector.x to 1 makes vector.array()[0] equal 1', () => {
+  const vector = new Vector(2, 3, 5);
+  vector.x = 1;
+  expect(vector.array()[0]).toBe(1);
+})
+
+test('vector.y gets 3 from vector <2, 3, 5>', () => {
+  const vector = new Vector(2, 3, 5);
+  expect(vector.y).toBe(3);
+})
+
+test('setting vector.y to 1 makes vector.array()[1] equal 1', () => {
+  const vector = new Vector(2, 3, 5);
+  vector.y = 1;
+  expect(vector.array()[1]).toBe(1);
+})
+
+test('vector.z gets 5 from vector <2, 3, 5>', () => {
+  const vector = new Vector(2, 3, 5);
+  expect(vector.z).toBe(5);
+})
+
+test('setting vector.z to 1 makes vector.array()[2] equal 1', () => {
+  const vector = new Vector(2, 3, 5);
+  vector.z = 1;
+  expect(vector.array()[2]).toBe(1);
+})
+
+test('distance from <1, 1, 1> to <4, 5, 1> is 5', () => {
+  const v1 = new Vector(1, 1, 1);
+  const v2 = new Vector(4, 5, 1); 
+  expect(v1.dist(v2)).toBe(5);
+})
+
+test('distance from <4, 5, 1> to <1, 1, 1> is 5', () => {
+  const v1 = new Vector(4, 5, 1); 
+  const v2 = new Vector(1, 1, 1);
+  expect(v1.dist(v2)).toBe(5);
+})
+
+test('distance from <4, 5, 1> to <4, 5, 1> is 0', () => {
+  const v1 = new Vector(4, 5, 1); 
+  const v2 = new Vector(4, 5, 1);
+  expect(v1.dist(v2)).toBe(0);
+})
+
+test('v1.equals(v2) is true if v1 and v2 have the same components', () => {
+  const v1 = new Vector(4, 5, 1); 
+  const v2 = new Vector(4, 5, 1);
+  expect(v1.equals(v2)).toBe(true);
+})
+
+test('v1.equals(v2) is false if only the z-components of v1 and v2 differ', () => {
+  const v1 = new Vector(4, 5, 1); 
+  const v2 = new Vector(4, 5, 2);
+  expect(v1.equals(v2)).toBe(false);
 })
