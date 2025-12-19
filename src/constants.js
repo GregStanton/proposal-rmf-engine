bcf6c42c27769af76fb47792ce53b271b13f063f
Greg Stanton <highermathhelp@gmail.com>
2025-12-18 20:07:07 -0700

    Add docs and TOLERANCE to constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,12 @@
-// See custom-shapes.js for constants that need to be included.
+/**
+ * @module constants
+ */
+
+/**
+ * A number constant representing a tolerance, for use in comparisons.
+ * @constant {number}
+ * @default
+ */
+export const TOLERANCE = 0.001;
+
+// See custom-shapes.js for other constants that need to be included.
